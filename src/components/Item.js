fe0512ba289d9254d58c99a1220f329583d1c348
Hope <hopemwaura39@gmail.com>
2024-08-06 17:55:09 +0300

    state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(true);
+  function addToCart() {
+    setCart(!cart);
+  }
+  const cartMOde = cart ? "" : "in-cart";
   return (
-    <li className="">
+    <li className={cartMOde}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>
+        {cart ? "Add to Cart" : "Remove from cart"}
+      </button>
     </li>
   );
 }
